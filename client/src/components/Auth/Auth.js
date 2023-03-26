c5c997e9c1d3f4e15eee06ffa4db80b9e60494fc
Deepak Patil <108725514+DeepakkPatil@users.noreply.github.com>
2023-03-26 22:02:10 +0530

    before auth

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,94 @@
+import React , { useState} from 'react'
+import { Avatar, Button, Paper, Grid,Typography,Container, TextField } from '@material-ui/core'
+import Icon from './Icon';
+import { GoogleLogin} from 'react-google-login'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import useStyles  from './styles' ;
+import Input from './Input'; // made custom input field
+
+const Auth = () => {
+    const classes =useStyles() ;
+    const [isSignup,setIsSignUp]= useState(false) ;
+    const [ showPassword,setShowPassword]= useState(false) ;
+
+    const handleSubmit=()=>{}
+    const handleChange=()=>{}
+    const handleShowPassword=()=>( setShowPassword((prevState)=> !prevState))
+    const switchMode=()=>{
+
+        setIsSignUp((prevState)=>!prevState) // using this we are toggling bw signup and signIn
+        handleShowPassword(false );
+    }
+    
+    const googleSuccess=async(res)=>{
+
+        console.log(res) ;
+    }
+    const googleFailure=(error)=>{
+        console.log(error) ;
+        console.log("Google signin Unsuccessfull. Try again Later")
+    }
+    
+    
+  return (
+     <Container component="main" maxWidth="xs">
+    <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.Avatar}>
+            <LockOutlinedIcon/>
+        </Avatar>
+        <Typography variant="h5">{isSignup?'Sign Up':'Sign In'}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+                <Grid container spacing={2}>
+                    {
+                        isSignup && (
+                            <>
+                         
+                            <Input name='firstName' label='firstName' handleChange={handleChange}  half />
+               
+                            <Input name='firstName' label='firstName' handleChange={handleChange}  half />
+                            
+                            </>
+                        )
+                    }
+                    <Input  name='email' label='Email Address' handleChange={handleChange} type='email' />
+                    <Input name='password' label='Password' handleChange={handleChange} type={ showPassword ? 'text' :'password'} handleShowPassword={handleShowPassword} />
+                    {
+                         isSignup && <Input name='confirmPassword' label='Repeat Password' handleChange={handleChange} type='password'/>
+                    }
+                    
+                </Grid>
+                <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>
+                        {
+                            isSignup ? 'Sign Up': 'Sign In'
+                        }
+                </Button>
+                <GoogleLogin 
+                    clientId='499565496401-hl9j161bimpggug36p506i8l5uvktm2v.apps.googleusercontent.com'
+                    render={(renderProps)=>(
+                        <Button className={classes.googleButton} color='primary' fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon/>} variant='contained'>
+                            Google Sign In
+                        </Button>
+                    )}
+                    onSuccess={googleSuccess}
+                    onFailure={googleFailure}
+                    cookiePolicy="single_host_origin"
+                />
+                <Grid container justify='flex-end' >
+                        <Grid item>
+                            <Button onClick={switchMode} >
+                                    {
+                                        isSignup? 'Already have an account? Sign In' : 'Dont have an account? Sign Up'
+                                    }
+                            </Button>
+                        </Grid>
+                </Grid>
+        </form>
+
+    </Paper>
+
+ </Container>
+  )
+}
+
+export default Auth
+
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,44 @@
+import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core'
+import { Link } from 'react-router-dom'
+import React from 'react'
+import useStyles from './styles'
+import memories from '../../images/memories.png';
+
+const Navbar = () => {
+
+      const classes = useStyles();
+      const user= null ;
+  return (
+    <div>
+        
+        <AppBar className={classes.appBar} position="static" color="inherit">
+        <div className={classes.brandContainer}>
+        <Typography component={Link} to='/' className={classes.heading} variant="h2" align="center" >Memories</Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
+        </div>
+        <Toolbar className={classes.toolbar}>
+            {
+                user ? (
+                <div className={classes.profile}>
+                    <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageurl}>
+                        { user.result.name.charAt(0)}
+                    </Avatar>
+                    <Typography className={classes.userName} variant='h6'>
+                        { user.result.name }
+                    </Typography>
+                    <Button variant='contained' className={classes.logout} color='secondary' onClick={()=>{}}>
+                        Logout
+                    </Button>
+                </div>):(
+                    <Button component={Link} to='/auth' variant='contained' color='primary'>
+                        SignIn
+                    </Button>
+                )
+            }
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+export default Navbar
