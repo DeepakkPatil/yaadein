5d2f0215d39fb5e09d3a12666d3f1f2950fd3b63
Deepak Patil <108725514+DeepakkPatil@users.noreply.github.com>
2023-05-04 01:44:54 +0530

    Wrapped

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const API= axios.create({ baseURL: 'https://yaadein-lifestorybook.vercel.app/'}) ;
 
+
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
