93f044f0506a032f05cae947345505dca8499701
Deepak Patil <108725514+DeepakkPatil@users.noreply.github.com>
2023-06-09 01:25:17 +0530

    flaskapi.js

--- client/src/api/flaskapi.js
+++ client/src/api/flaskapi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API= axios.create({ baseURL: 'https://flask-hello-world-psi-livid.vercel.app'}) ;
+const API= axios.create(PROCESS.env.FLASK_ID) ;
 
-export const fetchSentiment = (userId) => axios.post('https://flask-hello-world-psi-livid.vercel.app/sentiments/', userId);
+export const fetchSentiment = (userId) => axios.post(PROCESS.env.FLASK_SENTIMENT, userId);
 export const fetchTrends = () => API.get(`/trends`);
 export const fetchPosts = () => API.get(`/posts`);
